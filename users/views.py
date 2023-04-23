e9d0335228c4583bd0a3a132b3d815bdebe6e9ba
liorco15 <liorco2066@gmail.com>
2023-04-23 14:04:58 +0300

    BSPMC231-31 - filter user results

--- users/views.py
+++ users/views.py
@@ -3,5 +3,13 @@ from registration.models import UserData
 
 
 def user_list(request):
+    if request.method == 'POST':
+        if request.POST.get('expert'):
+            users = UserData.objects.filter(isexpert=True)
+            return render(request, 'users.html', {'users': users})
+        else:
+            users = UserData.objects.filter(isexpert=False)
+            return render(request, 'users.html', {'users': users})
+
     users = UserData.objects.all()
     return render(request, 'users.html', {'users': users})
