c32b8a664607eb59184023c8eed1ad196a782d14
liorco15 <liorco2066@gmail.com>
2023-04-23 12:20:15 +0300

    Update views.py

--- home_page/views.py
+++ home_page/views.py
@@ -3,6 +3,5 @@ from OurArticlesModel import articlesModel
 
 
 def home(request):
-    # df = articlesModel.findArticles("Sports")
-    # return render(request, 'home.html', {'data': df.iterrows()})
-    return render(request, 'home.html')
+    df = articlesModel.findArticles("Sports")
+    return render(request, 'home.html', {'data': df.iterrows()})
