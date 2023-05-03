7fc54702c7c4296a85272cd9b6740efac30864b9
Din Ezra <dinezra11@gmail.com>
2023-05-03 23:11:26 +0300

    BSPMC231-12: Optimize articles retrieval (creating cache)

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import ArticleCache
+
 
 # Register your models here.
+@admin.register(ArticleCache)
+class RequestDemoAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in
+                    ArticleCache._meta.get_fields()]
--- articles/views.py
+++ articles/views.py
@@ -5,7 +5,8 @@ from OurArticlesModel import articlesModel
 # Create your views here.
 def catalog(request):
     try:
-        df = articlesModel.findArticles("Sports")
+        engine = articlesModel.initializeEngine("Sports")
+        df = articlesModel.getPage(engine, 1)
         return render(request, 'articles/article.html', {'data': df.iterrows()})
     except:
         return render(request, 'articles/article.html')
