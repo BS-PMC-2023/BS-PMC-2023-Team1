0e516ce93c7dcba9db9132a24e2974992d4d9b29
Din Ezra <dinezra11@gmail.com>
2023-05-03 15:54:07 +0300

    Transfer home<->articles design

--- home_page/views.py
+++ home_page/views.py
@@ -1,12 +1,7 @@
 from django.shortcuts import render
-from OurArticlesModel import articlesModel
 from django.test import TestCase, Client
 from django.urls import reverse
 
 
 def home(request):
-    try:
-        df = articlesModel.findArticles("news")
-        return render(request, 'home.html', {'data': df.iterrows()})
-    except:
-        return render(request, 'home.html')
+    return render(request, 'home.html')
