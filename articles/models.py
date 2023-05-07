590b64a8fc46fcfabd413bc7bb8c647ec1c6c4f7
Din Ezra <dinezra11@gmail.com>
2023-05-07 11:56:12 +0300

    BSPMC231-12: Add approves table and like/unlike buttons

--- articles/models.py
+++ articles/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from registration.models import UserData
 
 
 # Create your models here.
@@ -7,3 +8,8 @@ class ArticleCache(models.Model):
     content = models.CharField(max_length=300, default="")
     img = models.CharField(max_length=200, default="")
     isFake = models.BooleanField(default=False)
+
+
+class PredictionApproves(models.Model):
+    link = models.CharField(max_length=200, default="unknown", primary_key=True)
+    expertId = models.ForeignKey(UserData, on_delete=models.CASCADE)
