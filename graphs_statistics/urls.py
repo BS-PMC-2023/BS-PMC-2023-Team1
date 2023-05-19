c5f8c9aec41e801364c5fa030f52de782ea125fe
Din Ezra <dinezra11@gmail.com>
2023-05-19 11:53:39 +0300

    BSPMC231-15: Add statstical graphs

--- graphs_statistics/urls.py
+++ graphs_statistics/urls.py
@@ -1,3 +1,4 @@
+
 from django.template.context_processors import static
 from django.urls import path
 
@@ -6,4 +7,5 @@ from . import views
 
 urlpatterns = [
     path('graph', views.graphPredictionapprovess, name='graph'),
-    ]
+    path('general', views.graphGeneral, name='general_graphs'),
+]
