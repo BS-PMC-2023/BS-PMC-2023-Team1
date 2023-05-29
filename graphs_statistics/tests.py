53861d98e540e60e368c1aae2456133a18f779e7
Shai Mastitz <shaima7@ac.sce.ac.il>
2023-05-29 23:09:44 +0300

    tests and fix pics

--- graphs_statistics/tests.py
+++ graphs_statistics/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
+from django.urls import reverse
+from registration.models import UserData
+from articles.models import PredictionApproves, ArticleCache
 
-# Create your tests here.
+
+from django.test import TestCase
+from registration.models import UserData
+from articles.models import PredictionApproves, ArticleCache
+from . import views
+
+class YourAppTests(TestCase):
+    def setUp(self):
+        # Set up any necessary objects or data required for the tests
+        # For example, create sample data in the database
+        pass
+
+    def test_modelsPredictionsGraph(self):
+        # Write test cases for the modelsPredictionsGraph function
+        # Example:
+        # 1. Test the function with valid inputs and check if it returns the expected result
+        # 2. Test the function with different scenarios (e.g., empty database, specific data) and check the results
+
+        # Example test case:
+        graph = views.modelsPredictionsGraph(None)
+        self.assertIsNotNone(graph)  # Check if the graph is generated successfully
+
+    def test_usersTypeGraph(self):
+        # Write test cases for the usersTypeGraph function
+        # Example:
+        # 1. Test the function with valid inputs and check if it returns the expected result
+        # 2. Test the function with different scenarios (e.g., empty database, specific data) and check the results
+
+        # Example test case:
+        graph = views.usersTypeGraph(None)
+        self.assertIsNotNone(graph)  # Check if the graph is generated successfully
+
+    # Add more test methods for other functions as needed
+
