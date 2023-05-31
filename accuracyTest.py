a8d3c2c8206d7b3617a0c140a0bd91eccf62910b
shaima77 <93545602+shaima77@users.noreply.github.com>
2023-05-31 23:25:44 +0300

    Update accuracyTest.py

--- accuracyTest.py
+++ accuracyTest.py
@@ -2,8 +2,7 @@ import pandas as pd
 from sklearn.metrics import accuracy_score
 
 
-def getAccuracy():
-    df = pd.read_csv('news data/test.csv')
-    predictions = getPrediction(df)
+df = pd.read_csv('news data/test.csv')
+predictions = getPrediction(df)
 
-    return accuracy_score(df['label'], predictions)
+print( accuracy_score(df['label'], predictions))
