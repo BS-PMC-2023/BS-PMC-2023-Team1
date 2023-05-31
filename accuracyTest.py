80f3231b6ef7e07ad5e8280736a4b9dc7a227c4e
shaima77 <93545602+shaima77@users.noreply.github.com>
2023-05-31 23:13:21 +0300

    Add files via upload

---
+++ accuracyTest.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import articlesModel
+from sklearn.metrics import accuracy_score
+
+
+def getAccuracy():
+    df = pd.read_csv('news data/test.csv')
+    predictions = articlesModel.getPrediction(df)
+
+    return accuracy_score(df['label'], predictions)
