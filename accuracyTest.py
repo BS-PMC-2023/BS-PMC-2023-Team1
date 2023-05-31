0d57d21b7bdbe78639a3827803a3975e36460652
shaima77 <93545602+shaima77@users.noreply.github.com>
2023-06-01 00:47:25 +0300

    Update accuracyTest.py

--- accuracyTest.py
+++ accuracyTest.py
@@ -1,7 +1,21 @@
 import pandas as pd
 from acc import getacc
+import pandas as pd
+from articlesModel import getPrediction
+from sklearn.metrics import accuracy_score
+
+
 
 def getAccuracy():
     df = pd.read_csv(r'test.csv')
     print(getacc(df))
 getAccuracy()
+
+
+
+def getAccuracy2():
+    df = pd.read_csv(r'test.csv')
+    predictions = articlesModel.getPrediction(df)
+
+    return accuracy_score(df['label'], predictions)
+print(getAccuracy2())
