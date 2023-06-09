4623ea86d0a91fa0ad603d822001b8e6d05276fd
hadaris <hadaris@ac.sce.ac.il>
2023-06-09 17:30:46 +0300

    nothing to see here
    just modifications of library versions. this is not relevant to all so will not be pushed to main

--- registration/admin.py
+++ registration/admin.py
@@ -1,13 +1,13 @@
-import here as here
-from django.contrib impmodels here
-from django.contrib import admin
-from .models import admin
-
-# Register your ort UserData
-
-
-# Register your models here.
-@admin.register(UserData)
-class RequestDemoAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in
-                    UserData._meta.get_fields()]
+# import here as here
+# from django.contrib impmodels here
+# from django.contrib import admin
+# from .models import admin
+#
+# # Register your ort UserData
+#
+#
+# # Register your models here.
+# @admin.register(UserData)
+# class RequestDemoAdmin(admin.ModelAdmin):
+#     list_display = [field.name for field in
+#                     UserData._meta.get_fields()]
