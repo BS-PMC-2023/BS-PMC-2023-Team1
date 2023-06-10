a1c95bf68f8db1c6ca16349bbbf7eb26aa6eda81
shaima77 <93545602+shaima77@users.noreply.github.com>
2023-06-10 12:22:35 +0300

    Update acc.py

--- acc.py
+++ acc.py
@@ -1,2 +1,22 @@
+import re
+import pickle
+import numpy as np
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+from keras.preprocessing.text import one_hot
+from keras.utils import pad_sequences
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, Dropout, Embedding
+nltk.download('stopwords')
+
 def getacc(df):
-  return "90%"
+  df.head()
+
+  
